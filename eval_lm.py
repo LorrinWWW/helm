7758510a14bc20a7622445bf3503830fb09a13af
Shaked Regev <shaked@abe95083-03.cloud.together.ai>
2024-04-03 16:45:01 -0700

    fixed weird errors for the long test, still need to run it

---
+++ eval_lm.py
@@ -0,0 +1,101 @@
+import torch
+import numpy as np
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+from datasets import load_dataset
+from trl.trainer import ConstantLengthDataset
+import tqdm
+from fire import Fire
+import sys
+from quanto import Calibration, freeze, qfloat8, qint4, qint8, quantize
+from helm.common.hierarchical_logger import htrack_block, hlog
+
+@torch.no_grad()
+def calibrate_func(model, tokenizer, batch_size, batches):
+    samples = batch_size * batches
+    cal_dataset = load_dataset("lambada", split=["validation"])[0]
+    model.eval()
+    total = 0
+    for batch in cal_dataset.iter(batch_size=batch_size):
+        inputs = tokenizer(batch["text"], return_tensors="pt", padding=True)
+        input_ids = inputs.input_ids.to(model.device)
+        attention_mask = inputs.attention_mask.to(model.device)
+        model(input_ids, attention_mask=attention_mask)
+        total += input_ids.size(0)
+        if total >= samples:
+            break
+
+
+def evaluate(
+    model_id: str,
+    data_name: str,
+    seq_length: int = 4096,
+    num_seqs: int = 256,
+):
+
+    if data_name == 'c4':
+
+        data = load_dataset('Jackmin108/c4-en-validation', split='validation')
+
+    elif data_name == 'wikitext':
+
+        data = load_dataset('wikitext', 'wikitext-103-raw-v1', split='test')
+    
+    else:
+
+        assert False
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+    data = ConstantLengthDataset(
+        tokenizer,
+        data,
+        dataset_text_field='text',
+        infinite=False,
+        shuffle=False,
+        seq_length=seq_length,
+        append_concat_token=False, 
+        add_special_tokens=False
+    )
+    with htrack_block(f"Loading Hugging Face model {model_id}"):
+        # WARNING this may fail if your GPU does not have enough memory
+        model = AutoModelForCausalLM.from_pretrained(
+            model_id, trust_remote_code=True, torch_dtype=torch.float16, device_map='auto'
+        )
+    
+    quantize(model, weights=qfloat8, activations=qfloat8)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
+    batch_size = 32
+    batches = 4
+    torch.backends.cudnn.enable = True
+    torch.backends.cudnn.benchmark = True
+    with Calibration(momentum=0.8):
+        calibrate_func(model, tokenizer, batch_size, batches)
+    freeze(model)
+    
+    losses = []
+    
+    for i, item in enumerate(tqdm.tqdm(data, total=num_seqs)):
+    
+        if i >= num_seqs:
+            break
+        
+        text = tokenizer.decode(item['input_ids'])
+    
+        input_ids = tokenizer(text, return_tensors='pt', add_special_tokens=False).input_ids.to(model.device)
+    
+        with torch.no_grad():
+            loss = model(input_ids, labels=input_ids).loss.item()
+            
+        losses.append(loss)
+    
+    print(np.mean(losses), np.exp(np.mean(losses)))
+
+
+# call the evaluate function with command line arguments
+if __name__ == '__main__':
+    model_id = sys.argv[1]
+    data_name = sys.argv[2]
+    evaluate(model_id, data_name)
+    
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,9 +1,12 @@
 export MODEL_NAME=mistralai/Mistral-7B-Instruct-v0.1
+#export MODEL_NAME=mistralai/Mixtral-8x7B-Instruct-v0.1
+
 export PULSAR_VERSION=1.0
 export PULSAR_MODEL_NAME="${MODEL_NAME}_${PULSAR_VERSION}"
 
 export BOS_TOKEN="<s>"
 export INSTANCES=10
+export SUITE_NAME="hf_v1"
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
@@ -17,7 +20,7 @@ sed "s#<<MODEL_NAME>>#${MODEL_NAME}#g" src/helm/benchmark/presentation/run_specs
 HELM_CLIENT_TYPE=hf helm-run -n 1 \
     -c src/helm/benchmark/presentation/run_specs_lite_core_p1p2_tmp.conf \
     --enable-huggingface-models ${MODEL_NAME} \
-    --suite hf_v1 \
+    --suite ${SUITE_NAME} \
     --max-eval-instances ${INSTANCES} 
 
 # HELM_CLIENT_TYPE=pulsar helm-run \
@@ -28,9 +31,9 @@ HELM_CLIENT_TYPE=hf helm-run -n 1 \
 
 wait
 
-helm-summarize --suite hf_v1
+helm-summarize --suite ${SUITE_NAME}
 #helm-summarize --suite pulsar_v1
 
 
-cat benchmark_output/runs/hf_v1/groups/latex/core_scenarios_accuracy.tex
+cat benchmark_output/runs/${SUITE_NAME}/groups/latex/core_scenarios_accuracy.tex
 #cat benchmark_output/runs/pulsar_v1/groups/latex/core_scenarios_accuracy.tex
