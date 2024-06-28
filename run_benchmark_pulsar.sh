6f680293a6d4b452ce57ac991f8cc061d0712bcf
Shaked Regev <shakedvregev@gmail.com>
2024-06-28 12:26:11 -0700

    pulsar commit

--- run_benchmark_pulsar.sh
+++ run_benchmark_pulsar.sh
@@ -2,8 +2,8 @@ export MODEL_NAME=${HELM_MODEL_NAME:-"unknown"}
 export PULSAR_VERSION=2.0
 export PULSAR_MODEL_NAME="${MODEL_NAME}_${PULSAR_VERSION}"
 
-export BOS_TOKEN="<s>"
-export INSTANCES=1000
+export BOS_TOKEN=""
+export INSTANCES=100
 export TRIALS=3
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
@@ -13,9 +13,9 @@ sed "s#<<MODEL_NAME>>#${MODEL_NAME}#g" src/helm/benchmark/presentation/run_specs
 HELM_CLIENT_TYPE=pulsar helm-run \
     -c src/helm/benchmark/presentation/run_specs_lite_core_p1p2_tmp.conf \
     --enable-huggingface-models $MODEL_NAME \
-    --suite pulsar_v2 \
+    --suite pulsar_fp8 \
     --max-eval-instances ${INSTANCES} -t ${TRIALS}
 
-helm-summarize --suite pulsar_v2
+helm-summarize --suite pulsar_fp8
 
-cat benchmark_output/runs/pulsar_v2/groups/latex/core_scenarios_accuracy.tex
+#cat benchmark_output/runs/pulsar_v2/groups/latex/core_scenarios_accuracy.tex
